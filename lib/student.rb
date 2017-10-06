29090f176fb804125d16d042c78346089be80285
John Tuttle <thejwt@gmail.com>
2017-10-06 21:44:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,25 +1,26 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
     if student_hash.has_key?(:name)
       :name = student_hash[:name]
-    
+    end
+    puts @name
+
   end
 
   def self.create_from_collection(students_array)
-    
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
