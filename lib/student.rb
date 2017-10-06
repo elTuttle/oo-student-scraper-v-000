8c48245000c56801f1101d097657a8f830a93d4f
John Tuttle <thejwt@gmail.com>
2017-10-06 21:58:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,6 +41,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       student = Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
