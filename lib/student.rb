f6e6cfad4358f06d064c3edb42a19716eb36ee97
John Tuttle <thejwt@gmail.com>
2017-10-06 21:49:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,13 +5,33 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    students_array = [@name,@location,@twitter,@linkedin,@github,@blog,@profile_quote,@bio,@profile_url]
-    students_array.each do |attribute|
-      if student_hash.has_key?(attribute)
-        attribute = student_hash[attribute]
-      end
+    if student_hash.has_key?(:name)
+      @name = student_hash[:name]
     end
-    puts @name
+    if student_hash.has_key?(:location)
+      @location = student_hash[:location]
+    end
+    if student_hash.has_key?(:twitter)
+      @twitter = student_hash[:twitter]
+    end
+    if student_hash.has_key?(:linkedin)
+      @linkedin = student_hash[:linkedin]
+    end
+    if student_hash.has_key?(:github)
+      @github = student_hash[:github]
+    end
+    if student_hash.has_key?(:blog)
+      @blog = student_hash[:blog]
+    end
+    if student_hash.has_key?(:profile_url)
+      @profile_url = student_hash[:profile_url]
+    end
+    if student_hash.has_key?(:bio)
+      @bio = student_hash[:bio]
+    end
+    if student_hash.has_key?(:profile_quote)
+      @profile_quote = student_hash[:profile_quote]
+    end
 
   end
 
