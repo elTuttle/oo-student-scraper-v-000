47488cd8539150d2aa9f9a95959a8c22f3bd0320
John Tuttle <thejwt@gmail.com>
2017-10-06 21:51:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,33 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-
+    if attributes_hash.has_key?(:name)
+      @name = attributes_hash[:name]
+    end
+    if attributes_hash.has_key?(:location)
+      @location = attributes_hash[:location]
+    end
+    if attributes_hash.has_key?(:twitter)
+      @twitter = attributes_hash[:twitter]
+    end
+    if attributes_hash.has_key?(:linkedin)
+      @linkedin = attributes_hash[:linkedin]
+    end
+    if attributes_hash.has_key?(:github)
+      @github = attributes_hash[:github]
+    end
+    if attributes_hash.has_key?(:blog)
+      @blog = attributes_hash[:blog]
+    end
+    if attributes_hash.has_key?(:profile_url)
+      @profile_url = attributes_hash[:profile_url]
+    end
+    if attributes_hash.has_key?(:bio)
+      @bio = attributes_hash[:bio]
+    end
+    if attributes_hash.has_key?(:profile_quote)
+      @profile_quote = attributes_hash[:profile_quote]
+    end
   end
 
   def self.all
