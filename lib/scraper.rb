da0538fafab42b6d9479604695999f18b28e7e54
John Tuttle <thejwt@gmail.com>
2017-10-06 20:28:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,45 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    people = []
+    #binding.pry
+    #doc.css(".student-name").first.text
+    #doc.css(".student-location").first.text
+    #doc.css("div.student-card a").attribute("href").value
+    doc.css(".student-card").each do |person|
+
+      people << {
+          :name => person.css(".student-name").text,
+          :location => person.css(".student-location").text,
+          :profile_url => person.css("a").attribute("href").value
+        }
+    end
+    people
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(profile_url))
+    student_bio = []
+    twitter = ""
+    linkedin = ""
+    github = ""
+    blog = ""
+    temp_string
+    #binding.pry
+    #doc.css(".vitals-text-container .profile-name").text
+    #doc.css(".social-icon-container a img").attribute("src").value
+    doc.css(".social-icon").each do |person|
+      temp_string = person.css("a")
+
+
+      binding.pry
+    end
+
   end
-
 end
-
